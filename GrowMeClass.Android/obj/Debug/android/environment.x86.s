b760aa89d496e83bcd889253612da1471ec1338e
Helene <180022@virtualwindow.co.za>
2020-03-31 19:04:30 +0200

    Sleep Page light button

--- GrowMeClass.Android/obj/Debug/android/environment.x86.s
+++ GrowMeClass.Android/obj/Debug/android/environment.x86.s
@@ -68,7 +68,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"f1e48a4a-d193-4163-b73a-b7eb8c3471cc"
+	.asciz	"c2fd93b0-c17d-4e03-bcee-d6c91d348df5"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
